463276a5f210c2df2edfd53507b34cc01ed9a9e6
marc0ssus <marcos.cunha@unidavi.edu.br>
2022-09-05 15:28:59 -0300

    changes

---
+++ JavaScript(Sandro)/labViagensCarro/README.md
@@ -0,0 +1,3 @@
+# viagens_carro
+
+cadastra viagens de carro, calcula a autonomia, e lista todas elas em seguida
--- JavaScript(Sandro)/labViagensCarro/index.php
+++ JavaScript(Sandro)/labViagensCarro/index.php
@@ -35,6 +35,7 @@
             <span class="input-group-text" id="basic-addon1">Destino:</span>
             <input type="text" class="form-control" aria-describedby="basic-addon1" id="destino" placeholder="Local de chegada" required>
         </div>
+        -
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Kilometragem percorrida:</span>
             <input type="number" class="form-control" aria-describedby="basic-addon1" id="km" placeholder="Km" required>
