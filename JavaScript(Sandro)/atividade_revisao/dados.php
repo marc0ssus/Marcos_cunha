7eed22ebb6499672de917cdefebc46d18f4ce948
marc0ssus <marcos.cunha@unidavi.edu.br>
2022-09-16 15:05:01 -0300

    atividade_revisao ajax version

--- JavaScript(Sandro)/atividade_revisao/dados.php
+++ JavaScript(Sandro)/atividade_revisao/dados.php
@@ -1,7 +1,7 @@
 <?php
    define("salariominimo",1212);
-   define("btnvoltar","<br><input type='button' onclick='history.go(-1)' value='Voltar'>");
    $erro = false;
+   $msg = "";
 
    function naoPreenchido($valor) {
     if ($valor == "") {
@@ -12,17 +12,18 @@
    }
    
    if (naoPreenchido($_POST["nome"])) {
-      echo "<p>Erro: Nome não informado</p>";
+      $msg = "<p>Erro: Nome não informado</p>";
       $erro = true;
    }
    
    if (naoPreenchido($_POST["endereco"])) {
-      echo "<p>Erro: Endereço não informado</p>";
+      $msg = $msg."<p>Erro: Endereço não informado</p>";
       $erro = true;
    }
 
    if (!$erro) {
-      printf("<strong>%s</strong> Recebe o equivalente a <strong>%1.2f salários mínimos</strong>. <br>Mora no endereço <strong>%s</strong><br>",$_POST["nome"],($_POST["salario"] / salariominimo),$_POST["endereco"]);
+      printf("<strong>%s</strong> Recebe o equivalente a <strong>%1.2f salários mínimos</strong>. <br>Mora no endereço:<br> <strong><pre>%s</pre></strong><br>",$_POST["nome"],($_POST["salario"] / salariominimo),$_POST["endereco"]);
+   } else {
+      echo $msg;
    }
-   echo btnvoltar;
 ?>
