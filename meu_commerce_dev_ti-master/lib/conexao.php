b4d38c55ea9d894a669b05f3275a8d348ec0ee64
marc0ssus <marcos.cunha@unidavi.edu.br>
2022-10-03 16:35:53 -0300

    delete de arquivos meu_commerce

--- meu_commerce_dev_ti-master/lib/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = 'root';
-$password = '';
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=meu_commerce', $username, $password);
-    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
--- meu_commerce_dev_ti-master/login.php
+++
@@ -1,9 +0,0 @@
-<form method="post">
-    Login: <br>
-    <input type="text" name="login" placeholder="e-mail">
-    <br>
-    Senha:<br>
-    <input type="password" name="senha">
-    <br>
-    <input type="submit" name="autenticar" value="Logar">
-</form>
